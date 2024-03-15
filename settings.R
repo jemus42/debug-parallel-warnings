b698fe3f53356b788e0ecee3ea3326490fdf78e4
Lukas Burk <lukas@quantenbrot.de>
2024-03-15 12:37:10 +0100

    ..

--- settings.R
+++ settings.R
@@ -7,7 +7,7 @@ data.table::setDTthreads(1)
 # Just to check that each instance actually produces the same data structure
 assert_result <- function(result) {
   checkmate::assert_data_table(result, nrows = n_repls)
-  checkmate::assert_names(result, identical.to = c("a", "b"))
+  checkmate::assert_true(identical(names(result), c("a", "b")))
   checkmate::assert_true(unique(result$a) == "blabla")
   checkmate::assert_true(unique(result$b) == 3)
 }
